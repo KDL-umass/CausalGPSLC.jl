e8974bd5738961352b56d7abcaf251df50203774
SamWitty <samawitty@gmail.com>
2019-12-10 17:33:21 -0500

    Refactored model to be consistent with overleaf doc

--- analysis/synthetic.jl
+++ analysis/synthetic.jl
@@ -15,31 +15,31 @@ using .Model
 # # Synthetic Data Generators
 
 # +
-function simLinearData(SigmaU, xNoise, yNoise, uNoise, UXslope, UYslope, XYslope)
+function simLinearData(SigmaU, tNoise, yNoise, uNoise, UTslope, UYslope, TYslope)
     n = size(SigmaU)[1]
     U = zeros(n)
-    X = zeros(n)
+    T = zeros(n)
     Y = zeros(n)
     epsY = zeros(n)
     
     U = mvnormal(zeros(n), SigmaU * uNoise)
     
     for i in 1:n
-        X[i] = normal(U[i] * UXslope, xNoise)
-        Ymean = (X[i] * XYslope + U[i] * UYslope)
+        T[i] = normal(U[i] * UTslope, tNoise)
+        Ymean = (T[i] * TYslope + U[i] * UYslope)
         epsY[i] = normal(0, yNoise)
         Y[i] = Ymean + epsY[i]
     end
-    return U, X, Y, epsY
+    return U, T, Y, epsY
 end
 
-function simLinearIntData(U, epsY, doX, XYslope)
+function simLinearIntData(U, epsY, doT, TYslope)
     n = length(U)
     
     Yint = zeros(n)
     
     for i in 1:n
-        Ymean = (XYslope * doX)
+        Ymean = (TYslope * doT)
         Yint[i] = Ymean + epsY[i]
     end
     return Yint
@@ -47,16 +47,15 @@ end
 
 # +
 # n should be even
-n = 100
+n = 10
 eps = 0.0000000000001
 uNoise = .5
-xNoise = 1.
+tNoise = 1.
 yNoise = 0.1
 
-
-UXslope = 1.
+UTslope = 1.
 UYslope = -1.
-XYslope = 1.
+TYslope = 1.
 
 
 # All individuals indepedendent.
@@ -81,8 +80,8 @@ end
 println("")
 # -
 
-U, X, Y, epsY = simLinearData(SigmaU, xNoise, yNoise, uNoise, UXslope, UYslope, XYslope)
-scatter(X, Y, c=U)
+U, T, Y, epsY = simLinearData(SigmaU, tNoise, yNoise, uNoise, UTslope, UYslope, TYslope)
+scatter(T, Y, c=U)
 colorbar()
 title("Color = U")
 xlabel("Treatment")
@@ -93,26 +92,25 @@ ylabel("Outcome")
 
 hyperparams = Dict()
 
-hyperparams["xNoiseMin"], hyperparams["xNoiseMax"] = 0.01, 3.
+hyperparams["tNoiseMin"], hyperparams["tNoiseMax"] = 0.01, 3.
 hyperparams["yNoiseMin"], hyperparams["yNoiseMax"] = 0.01, 3.
 hyperparams["uNoiseMin"], hyperparams["uNoiseMax"] = 0.01, 3.
 
-hyperparams["uxLSShape"], hyperparams["uxLSScale"] = 4., 4.
+hyperparams["utLSShape"], hyperparams["utLSScale"] = 4., 4.
 hyperparams["uyLSShape"], hyperparams["uyLSScale"] = 4., 4.
-hyperparams["xyLSShape"], hyperparams["xyLSScale"] = 4., 4.
+hyperparams["tyLSShape"], hyperparams["tyLSScale"] = 4., 4.
 
-hyperparams["xScaleShape"], hyperparams["xScaleScale"] = 4., 4.
+hyperparams["tScaleShape"], hyperparams["tScaleScale"] = 4., 4.
 hyperparams["yScaleShape"], hyperparams["yScaleScale"] = 4., 4.
 
 hyperparams["SigmaU"] = SigmaU
-
 load_generated_functions()
-(tr, _) = generate(AdditiveNoiseGPROC, (hyperparams,))
-generatedU = get_choices(tr)[:U]
-generatedX = get_choices(tr)[:X]
-generatedY = get_choices(tr)[:Y]
+(trace, _) = generate(AdditiveNoiseGPROC, (hyperparams,))
+generatedU = get_choices(trace)[:U]
+generatedT = get_choices(trace)[:Tr]
+generatedY = get_choices(trace)[:Y]
 
-scatter(generatedX, generatedY, c=generatedU)
+scatter(generatedT, generatedY, c=generatedU)
 colorbar()
 title("Color = Generated U")
 xlabel("Generated Treatment")
@@ -123,15 +121,15 @@ ylabel("Generated Outcome")
 nOuter = 1000
 nMHInner = 1
 nESInner = 1
-PosteriorSamples, tr = AdditiveNoisePosterior(hyperparams, X, Y, nOuter, nMHInner, nESInner)
+PosteriorSamples, trace = AdditiveNoisePosterior(hyperparams, T, Y, nOuter, nMHInner, nESInner)
 println("")
 
 # +
 # Estimation
 burnIn = 100
 MHStep = 10
-doX = -2.
-MeanSATEs, VarSATEs = SATE(PosteriorSamples[burnIn:MHStep:end], X, Y, doX)
+doT = -2.
+MeanSATEs, VarSATEs = SATE(PosteriorSamples[burnIn:MHStep:end], T, Y, doT)
 
 nSamplesPerMixture = 100
 
@@ -139,21 +137,15 @@ samples = SATEsamples(MeanSATEs, VarSATEs, nSamplesPerMixture)
 println("")
 
 # +
-intY = simLinearIntData(U, epsY, doX, XYslope)
+intY = simLinearIntData(U, epsY, doT, TYslope)
 
 # kdeplot(intY - Y, label="(Y|do(X=$doX)) - Y", c="r")
 axvline(sum(intY - Y)/n, c="r", ymax=0.1, label="Ground Truth")
 kdeplot(samples, label="GPROC Estimate")
 
-# kdeplot(intY, label="(Y|do(X=$doX))", c="b")
-# axvline(sum(intY1)/n, c="b", ymax=0.1)
-
-# kdeplot(Y, label="Y", c="black")
-# axvline(sum(Y)/n, c="black", ymax=0.1)
-
-
-# hist(intY, label="(Y|do(X=$doX))")
-
 legend()
 xlabel("SATE")
 ylabel("P(SATE)")
+# -
+
+
