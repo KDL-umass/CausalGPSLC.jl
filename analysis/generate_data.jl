d1cca0ac82cae9415c54726c145feefed3db92be
Kenta Takatsu <kt426@cornell.edu>
2019-12-13 01:50:42 -0500

    synthetic data generator

--- analysis/generate_data.jl
+++ analysis/generate_data.jl
@@ -1,9 +1,14 @@
 module GenerateData
 
 import TOML
+using StatsBase
+using LinearAlgebra
+using Base
+using Gen
 
 include("synthetic.jl")
 using .Synthetic
+export generate_synthetic
 
 function generate_synthetic(config_path::String)
     """
@@ -11,28 +16,50 @@ function generate_synthetic(config_path::String)
     """
     config = TOML.parsefile(config_path)
     n = config["data"]["n"]
-    num_obj = config["data"]["num_obj"]
+    obj_size = config["data"]["obj_size"]
     eps = config["data"]["eps"]
     ucov = config["data"]["ucov"]
-    dtype = config["data"]["dataType"]
+    xvar = config["data"]["xvar"]
 
-    # variance for data
+    # variance for data (used in additive noise model)
     tNoise = config["data"]["tNoise"]
     xNoise = config["data"]["xNoise"]
     uNoise = config["data"]["uNoise"]
     yNoise = config["data"]["yNoise"]
 
-    SigmaU = generateSigmaU(n, [num_obj for i in 1:n/num_obj], eps, ucov)
-    U, T, X, Y, epsY = Nothing, Nothing, Nothing, Nothing, Nothing
+    SigmaU = generateSigmaU(n, [obj_size for i in 1:n/obj_size], eps, ucov)
+    SigmaX = generateSigmaX(n, xvar, eps)
 
-    if dtype == "linear"
-        # parameter for linear data
-        UTslope = config["data"]["linear"]["UTslope"]
-        UYslope = config["data"]["linear"]["UYslope"]
-        TYslope = config["data"]["linear"]["TYslope"]
-        U, T, Y, epsY = simLinearData(SigmaU, tNoise, yNoise, uNoise, UTslope, UYslope, TYslope)
-    end
-    return SigmaU, U, T, X, Y, epsY
+    n = size(SigmaU)[1]
+    X = mvnormal(zeros(n), SigmaX * xNoise)
+    U = mvnormal(zeros(n), SigmaU * uNoise)
+
+    T, Y, epsY = NaN, NaN, NaN
+    ft, ftx, ftxu = NaN, NaN, NaN  # causal queries
+
+    # assignment for T
+    dtypex = config["data"]["XTAssignment"]
+    xtparams = config["data"]["XTparams"]
+    dtypeu = config["data"]["UTAssignment"]
+    utparams = config["data"]["UTparams"]
+
+    T = generateT(X, U, dtypex, dtypeu, xtparams, utparams, xNoise)
+
+    dtypex = config["data"]["XYAssignment"]
+    xyparams = config["data"]["XYparams"]
+    dtypeu = config["data"]["UYAssignment"]
+    uyparams = config["data"]["UYparams"]
+    dtypet = config["data"]["TYAssignment"]
+    typarams = config["data"]["TYparams"]
+
+    Y, epsY = generateY(X, U, T, dtypex, dtypeu, dtypet, xyparams, uyparams, typarams, yNoise)
+
+    ft = generate_ft(dtypet, typarams)
+    ftx = generate_ftx(dtypet, dtypex, typarams, xyparams)
+    ftxu = generate_ftxu(dtypet, dtypex, dtypeu, typarams, xyparams, uyparams)
+
+    causal_query = collect((ft, ftx, ftxu))
+    return SigmaU, U, T, X, Y, epsY, causal_query
 end
 
 end
