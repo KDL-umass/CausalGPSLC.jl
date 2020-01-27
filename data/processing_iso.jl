cc1ff96f7036b7b2a92537ae53470085bb15f9c8
SamWitty <samawitty@gmail.com>
2020-01-27 14:54:53 -0500

    Added bias experiments for NEED benchmark

--- data/processing_iso.jl
+++ data/processing_iso.jl
@@ -36,34 +36,34 @@ export generateImportanceWeights, resampleData
 function generateImportanceWeights(newMeans, newVars, weekday_df)
     importanceWeights = Dict()
 
-    for region in keys(newMeans)
-        is_region = weekday_df[!, :Region] .== region
-        temp_data = weekday_df[is_region, :][!, :DryBulbTemp]
+    for state in keys(newMeans)
+        is_state = weekday_df[!, :State] .== state
+        temp_data = weekday_df[is_state, :][!, :DryBulbTemp]
         ik = InterpKDE(kde(temp_data))
         oldPDFs = pdf(ik, temp_data)
-        newDist = Truncated(Normal(newMeans[region], newVars[region]), minimum(temp_data), maximum(temp_data))
+        newDist = Truncated(Normal(newMeans[state], newVars[state]), minimum(temp_data), maximum(temp_data))
         newPDFs = pdf.(newDist, temp_data)
-        importanceWeights[region] = Weights(newPDFs ./ oldPDFs)
+        importanceWeights[state] = Weights(newPDFs ./ oldPDFs)
     end
     
     return importanceWeights
 end
 
 function resampleData(nSamplesPerState, importanceWeights, weekday_df)
-    indeces = [i for i in 1:218]
-    regions = keys(importanceWeights)
-    n = length(regions) * nSamplesPerState
+    indeces = [i for i in 1:length(importanceWeights["CT"])]
+    states = keys(importanceWeights)
+    n = length(states) * nSamplesPerState
     
     T = zeros(n)
     Y = zeros(n)
     
     i = 1
     
-    for region in regions
-        is_region = weekday_df[!, :Region] .== region
-        region_data = weekday_df[is_region, :]
-        is_sampled = sample(indeces, importanceWeights[region], nSamplesPerState, replace=false)
-        sampled_data = region_data[is_sampled, :]
+    for state in states
+        is_state = weekday_df[!, :State] .== state
+        state_data = weekday_df[is_state, :]
+        is_sampled = sample(indeces, importanceWeights[state], nSamplesPerState, replace=false)
+        sampled_data = state_data[is_sampled, :]
         T[i:i+nSamplesPerState-1] = sampled_data[!, :DryBulbTemp]
         Y[i:i+nSamplesPerState-1] = sampled_data[!, :RealTimeDemand]
         i += nSamplesPerState
@@ -71,11 +71,11 @@ function resampleData(nSamplesPerState, importanceWeights, weekday_df)
 
     eps = 1e-7
     cov = 1.
-    nIndividualsArray = [nSamplesPerState for i in 1:length(regions)]
+    nIndividualsArray = [nSamplesPerState for i in 1:length(states)]
     sigmaU = generateSigmaU(n, nIndividualsArray, eps, cov)  
-    regions_key = vcat(fill.(regions, nSamplesPerState)...)
+    states_key = vcat(fill.(states, nSamplesPerState)...)
     
-    return T, Y, sigmaU, regions_key
+    return T, Y, sigmaU, states_key
 end
 
 end
