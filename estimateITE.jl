d4d7a452edb1d8554d0d8eb0cce8d58adc2097ef
Jack Kenney <jack@kenney.dev>
2022-01-31 12:50:32 -0500

    update data import syntax

--- estimateITE.jl
+++ estimateITE.jl
@@ -5,6 +5,7 @@ Pkg.activate("GPSLCenv")
 println("Adding packages")
 using Random
 using CSV
+using DataFrames
 using ArgParse
 using DataFrames
 using LinearAlgebra
@@ -29,152 +30,152 @@ function parse_commandline()
     s = ArgParseSettings()
     @add_arg_table! s begin
         "--datapath"
-            help = "a path to the data"
-            default = "data/NEEC_sampled.csv"
-            arg_type = String
+        help = "a path to the data"
+        default = "data/NEEC_sampled.csv"
+        arg_type = String
         "--output_filepath"
-            help = "filepath for inference results"
-            default = "results/NEEC_sampled_80.csv"
-            arg_type = String
+        help = "filepath for inference results"
+        default = "results/NEEC_sampled_80.csv"
+        arg_type = String
         "--doT"
-            help = "treatment value to intervene"
-            default = 0.8
-            arg_type = Float64
+        help = "treatment value to intervene"
+        default = 0.8
+        arg_type = Float64
         # posterior updates
         "--nOuter"
-            help = "the number of posterior steps"
-            default = 25
-            arg_type = Int
+        help = "the number of posterior steps"
+        default = 25
+        arg_type = Int
         "--nMHInner"
-            help = "the number of metropolis hastings sampling steps"
-            default = 1
-            arg_type = Int
+        help = "the number of metropolis hastings sampling steps"
+        default = 1
+        arg_type = Int
         "--nESInner"
-            help = "the number of elliptical slice sampling steps"
-            default = 1
-            arg_type = Int
+        help = "the number of elliptical slice sampling steps"
+        default = 1
+        arg_type = Int
         "--nU"
-            help = "the dimension of latent confounders to model"
-            default = 1
-            arg_type = Int
+        help = "the dimension of latent confounders to model"
+        default = 1
+        arg_type = Int
         # inference
         "--burnIn"
-            help = "the number of posterior samples for burn-in"
-            default = 10
-            arg_type = Int
+        help = "the number of posterior samples for burn-in"
+        default = 10
+        arg_type = Int
         "--stepSize"
-            help = "the step size during the inference step"
-            default = 1
-            arg_type = Int
+        help = "the step size during the inference step"
+        default = 1
+        arg_type = Int
         "--samplesPerPost"
-            help = "the number of samples from each posterior for treatment effect approximation"
-            default = 10
-            arg_type = Int
+        help = "the number of samples from each posterior for treatment effect approximation"
+        default = 10
+        arg_type = Int
         # parameters for priors
         "--uNoiseShape"
-            help = "the shape parameter of the prior inv gamma over the noise of U"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over the noise of U"
+        default = 4.0
+        arg_type = Float64
         "--uNoiseScale"
-            help = "the scale parameter of the prior inv gamma over the noise of U"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over the noise of U"
+        default = 4.0
+        arg_type = Float64
         "--xNoiseShape"
-            help = "the shape parameter of the prior inv gamma over the noise of X"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over the noise of X"
+        default = 4.0
+        arg_type = Float64
         "--xNoiseScale"
-            help = "the scale parameter of the prior inv gamma over the noise of X"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over the noise of X"
+        default = 4.0
+        arg_type = Float64
         "--tNoiseShape"
-            help = "the shape parameter of the prior inv gamma over the noise of T"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over the noise of T"
+        default = 4.0
+        arg_type = Float64
         "--tNoiseScale"
-            help = "the scale parameter of the prior inv gamma over the noise of T"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over the noise of T"
+        default = 4.0
+        arg_type = Float64
         "--yNoiseShape"
-            help = "the shape parameter of the prior inv gamma over the noise of Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over the noise of Y"
+        default = 4.0
+        arg_type = Float64
         "--yNoiseScale"
-            help = "the scale parameter of the prior inv gamma over the noise of Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over the noise of Y"
+        default = 4.0
+        arg_type = Float64
         "--xScaleShape"
-            help = "the shape parameter of the prior inv gamma over kernel scale of X"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel scale of X"
+        default = 4.0
+        arg_type = Float64
         "--xScaleScale"
-            help = "the scale parameter of the prior inv gamma over kernel scale of X"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel scale of X"
+        default = 4.0
+        arg_type = Float64
         "--tScaleShape"
-            help = "the shape parameter of the prior inv gamma over kernel scale of T"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel scale of T"
+        default = 4.0
+        arg_type = Float64
         "--tScaleScale"
-            help = "the scale parameter of the prior inv gamma over kernel scale of T"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel scale of T"
+        default = 4.0
+        arg_type = Float64
         "--yScaleShape"
-            help = "the shape parameter of the prior inv gamma over kernel scale of Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel scale of Y"
+        default = 4.0
+        arg_type = Float64
         "--yScaleScale"
-            help = "the scale parameter of the prior inv gamma over kernel scale of Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel scale of Y"
+        default = 4.0
+        arg_type = Float64
         "--uxLSShape"
-            help = "the shape parameter of the prior inv gamma over kernel lengthscale of U and X"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel lengthscale of U and X"
+        default = 4.0
+        arg_type = Float64
         "--uxLSScale"
-            help = "the scale parameter of the prior inv gamma over kernel lengthscale of U and X"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel lengthscale of U and X"
+        default = 4.0
+        arg_type = Float64
         "--utLSShape"
-            help = "the shape parameter of the prior inv gamma over kernel lengthscale of U and T"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel lengthscale of U and T"
+        default = 4.0
+        arg_type = Float64
         "--utLSScale"
-            help = "the scale parameter of the prior inv gamma over kernel lengthscale of U and T"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel lengthscale of U and T"
+        default = 4.0
+        arg_type = Float64
         "--xtLSShape"
-            help = "the shape parameter of the prior inv gamma over kernel lengthscale of X and T"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel lengthscale of X and T"
+        default = 4.0
+        arg_type = Float64
         "--xtLSScale"
-            help = "the scale parameter of the prior inv gamma over kernel lengthscale of X and T"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel lengthscale of X and T"
+        default = 4.0
+        arg_type = Float64
         "--uyLSShape"
-            help = "the shape parameter of the prior inv gamma over kernel lengthscale of U and Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel lengthscale of U and Y"
+        default = 4.0
+        arg_type = Float64
         "--uyLSScale"
-            help = "the scale parameter of the prior inv gamma over kernel lengthscale of U and Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel lengthscale of U and Y"
+        default = 4.0
+        arg_type = Float64
         "--xyLSShape"
-            help = "the shape parameter of the prior inv gamma over kernel lengthscale of X and Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel lengthscale of X and Y"
+        default = 4.0
+        arg_type = Float64
         "--xyLSScale"
-            help = "the scale parameter of the prior inv gamma over kernel lengthscale of X and Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel lengthscale of X and Y"
+        default = 4.0
+        arg_type = Float64
         "--tyLSShape"
-            help = "the shape parameter of the prior inv gamma over kernel lengthscale of T and Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the shape parameter of the prior inv gamma over kernel lengthscale of T and Y"
+        default = 4.0
+        arg_type = Float64
         "--tyLSScale"
-            help = "the scale parameter of the prior inv gamma over kernel lengthscale of T and Y"
-            default = 4.0
-            arg_type = Float64
+        help = "the scale parameter of the prior inv gamma over kernel lengthscale of T and Y"
+        default = 4.0
+        arg_type = Float64
     end
     return parse_args(s)
 end
@@ -185,7 +186,7 @@ function main()
     println("Parsing Arguments")
     parsed_args = parse_commandline()
     csv_path = parsed_args["datapath"]
-    df = CSV.read(csv_path)
+    df = CSV.read(csv_path, DataFrame)
 
     # build a list of object size
     # [a, a, a, b, c, c] -> [3, 1, 2]
@@ -209,9 +210,9 @@ function main()
     Y = Array(df[!, :Y])
 
     cols = names(df)
-    cols = deleteat!(cols, cols .== :T)
-    cols = deleteat!(cols, cols .== :Y)
-    cols = deleteat!(cols, cols .== :obj)
+    cols = deleteat!(cols, cols .== "T")
+    cols = deleteat!(cols, cols .== "Y")
+    cols = deleteat!(cols, cols .== "obj")
     if length(cols) == 0
         X = nothing
     else
@@ -236,14 +237,14 @@ function main()
     samplesPerPost = parsed_args["samplesPerPost"]
 
     println("Estimating ITE")
-    ITEsamples = zeros(length(T), samplesPerPost*length(burnIn:stepSize:nOuter)) # output in Algorithm 3
+    ITEsamples = zeros(length(T), samplesPerPost * length(burnIn:stepSize:nOuter)) # output in Algorithm 3
     idx = 1
     for i in tqdm(burnIn:stepSize:nOuter)
         uyLS = []
         U = []
         for u in 1:nU
-            push!(uyLS, posteriorsample[i][:uyLS => u => :LS])
-            push!(U, posteriorsample[i][:U => u => :U])
+            push!(uyLS, posteriorsample[i][:uyLS=>u=>:LS])
+            push!(U, posteriorsample[i][:U=>u=>:U])
         end
 
         doT = parsed_args["doT"]
@@ -256,31 +257,31 @@ function main()
         uyLS = convert(Array{Float64,1}, uyLS)
 
         MeanITE, CovITE = conditionalITE(uyLS,
-                          posteriorsample[i][:tyLS],
-                          xyLS,
-                          posteriorsample[i][:yNoise],
-                          posteriorsample[i][:yScale],
-                          U,
-                          X,
-                          T,
-                          Y,
-                          doT)
+            posteriorsample[i][:tyLS],
+            xyLS,
+            posteriorsample[i][:yNoise],
+            posteriorsample[i][:yScale],
+            U,
+            X,
+            T,
+            Y,
+            doT)
 
         for j in 1:samplesPerPost
-            samples = rand(MvNormal(MeanITE, Symmetric(CovITE) + I*(1e-10)))
+            samples = rand(MvNormal(MeanITE, Symmetric(CovITE) + I * (1e-10)))
             ITEsamples[:, idx] = samples
             idx += 1
         end
     end
     # n x m matrix where n is the number of data,
     # and m is the number of samples
-    
-    meanITE = mean(ITEsamples, dims=2)[:, 1]
+
+    meanITE = mean(ITEsamples, dims = 2)[:, 1]
     lowerITE = broadcast(quantile, [ITEsamples[i, :] for i in 1:size(ITEsamples)[1]], 0.05)
     upperITE = broadcast(quantile, [ITEsamples[i, :] for i in 1:size(ITEsamples)[1]], 0.95)
 
 
-    df = DataFrame(Individual=1:size(meanITE)[1], Mean=meanITE, LowerBound=lowerITE, UpperBound=upperITE)
+    df = DataFrame(Individual = 1:size(meanITE)[1], Mean = meanITE, LowerBound = lowerITE, UpperBound = upperITE)
     CSV.write(parsed_args["output_filepath"], df)
     println("Saved ITE mean and 90% credible intervals to " * parsed_args["output_filepath"])
 end
