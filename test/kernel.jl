8126684d1702cb21fbf193cc2013d7f431b8071b
Jack Kenney <jack@kenney.dev>
2022-03-05 15:09:34 -0500

    add tests

--- test/kernel.jl
+++ test/kernel.jl
@@ -12,6 +12,6 @@
         @test logit(0.5) == 0
     end
     @testset "expit" begin
-
+        @test expit(0) == 0.5
     end
 end
--- test/utils.jl
+++ test/utils.jl
@@ -3,6 +3,9 @@
 
     end
     @testset "removeAdjacent" begin
-
+        input = [1, 2, 2, 3, 4, 4, 5, 3, 4]
+        expected = [1, 2, 3, 4, 5, 3, 4]
+        actual = removeAdjacent(input)
+        @test expected == actual
     end
 end
