905d232779ca790301493976a9772c207ae5be8f
Jack Kenney <jack@kenney.dev>
2022-03-23 18:41:08 -0400

    add Bonferroni correction

--- test/sbc.jl
+++ test/sbc.jl
@@ -1,4 +1,4 @@
-function simulationBasedCalibration(prior, likelihood, posterior, numTrials, numSamples)
+function simulationBasedCalibration(prior, likelihood, posterior, numTrials, numSamples; confidence=0.05)
     theta = prior() # draw "true" theta from prior 
     numParams = size(theta, 1) # theta is 1D
     quantileSamples = zeros((numTrials, numParams))
@@ -10,15 +10,15 @@ function simulationBasedCalibration(prior, likelihood, posterior, numTrials, num
 
         # posterior returns array of samples (e.g. every 100th MH step)
         thetaSamples = posterior(y, numSamples) # numSamples x numParams
+        @assert size(thetaSamples) == (numSamples, numParams)
 
         for h = 1:numParams
             # where does this true theta component fall in the sampled distribution?
-            quantileSamples[t, h] = thetaQuantile(thetaSamples[:,h], theta[h])
+            quantileSamples[t, h] = thetaQuantile(thetaSamples[:, h], theta[h])
         end
     end
-    # return quantileSamples
     # check if quantiles are uniformly distributed
-    if isApproxUniform(quantileSamples, numTrials, numSamples)
+    if isApproxUniform(quantileSamples, numTrials, numSamples, confidence)
         return true
     end
     false
@@ -38,33 +38,34 @@ function thetaQuantile(dist, theta)
     return dist[end]
 end
 
-function isApproxUniform(dist, numTrials, numSamples)
-    # https://en.wikipedia.org/wiki/Bonferroni_correction#Definition
-    # https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
-    dist = vec(dist) 
+function isApproxUniform(dist, numTrials, numSamples, confidence)
+    numParams = size(dist, 2)
+    @test numTrials == size(dist, 1)
 
-    # trials x params
-    # p-values for each parameter
-    # apply B.correction over all parameters
+    for h in 1:numParams
+        d = Distributions.Uniform(0, numSamples)
+        # https://en.wikipedia.org/wiki/Kolmogorov%E2%80%93Smirnov_test
+        T = HypothesisTests.ApproximateOneSampleKSTest(dist[:, h], d)
+        p = HypothesisTests.pvalue(T)
+        # https://en.wikipedia.org/wiki/Bonferroni_correction#Definition
+        if p > (confidence / numParams) # Bonferroni correction
+            return false
+        end
+    end
 
-    d = Distributions.Uniform(0, numSamples)
-    T = HypothesisTests.ApproximateOneSampleKSTest(dist, d)
-    p = HypothesisTests.pvalue(T)
-    return p < 0.05
+    return true # reject null hypothesis
 end
 
 @testset "Simulation-Based Calibration" begin
-    # SBC: Run univariate sbc for each dimension
-    # Apply Bonferroni correction to adjust p-value (multiple hypothesis correction)
     # https://statmodeling.stat.columbia.edu/2021/09/03/simulation-based-calibration-some-challenges-and-directions-for-future-research/
 
     @testset "Verify SBC procedure with prior" begin
         numSamples = 100
         numTrials = 100 * numSamples
         @gen prior() = [randn()]
-        numParams = size(prior(),1)
+        numParams = size(prior(), 1)
         @gen likelihood(theta) = [randn() * theta]
-        @gen posterior(y, numSamples) = reshape([prior()[1] for i=1:numSamples], (numSamples,numParams))
+        @gen posterior(y, numSamples) = reshape([prior()[1] for i = 1:numSamples], (numSamples, numParams))
 
         @test simulationBasedCalibration(prior, likelihood, posterior, numTrials, numSamples)
     end
