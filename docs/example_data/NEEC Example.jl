6ec12e757380a36ba79d658a5908b0ff9196bd43
Jack Kenney <jack@kenney.dev>
2022-05-17 17:08:28 -0400

    finalize neec example

--- docs/example_data/NEEC Example.jl
+++ docs/example_data/NEEC Example.jl
@@ -1,35 +1,32 @@
 import Random # hide
 Random.seed!(1234) # hide
+using GPSLC # hide
+using Plots # hide
+using Statistics # hide
 
-using GPSLC
-using Plots
-using Statistics
-
+# run inference
 dataFile = "docs/example_data/NEEC_sampled.csv"
 g = gpslc(dataFile)
 
-
-
+# collect counterfactual outcomes
 nsamples = 30
 numPosteriorSamples = getNumPosteriorSamples(g)
-
-cfs = zeros(getN(g), getN(g), nsamples * numPosteriorSamples)
-
-for (i, doT) in enumerate(g.T)
-    cfs[i, :, :] = predictCounterfactualOutcomes(g, doT, nsamples)
-end
-
-ite = mean(cfs, dims=[2])[:, 1, :]
-s = summarizeITE(ite)
-
-plot(legend=:outertopright, size=(750, 400))
 o = "MA"
-agg = CSV.read("test/test_data/NEEC_aggregated.csv", DataFrame)
-idx = vec(agg[!, "obj"] .== o)
-scatter!(agg[idx, "T"], agg[idx, "Y"] .* 10, label="agg", makershape=:triangle)
 idx = vec(g.obj .== o)
-scatter!(g.T[idx] .* 100, g.Y[idx] .* 10, label="$(o) original", markershape=:circle)
-scatter!(g.T[idx] .* 100, s[!, "Mean"][idx] .* 10, label="$(o) doT", markershape=:diamond)
+ite, doT = predictCounterfactualEffects(g, nsamples; fidelity=10)
+YcfMA = mean(g.Y[idx]) .+ ite[:, idx, :]
+
+# get credible interval on counterfactual outcomes
+sate = mean(YcfMA, dims=2)[:, 1, :]
+s = summarizeEstimates(sate)
+
+# plot outcomes and credible interval
+order = sortperm(doT)
+plot(legend=:outertopright, size=(750, 400))
+scatter!(g.T[idx] .* 100, g.Y[idx] .* 10, label="$(o) obs", markershape=:circle)
+plot!(doT[order] .* 100, s[!, "Mean"][order] .* 10,
+    ribbon=(s[!, "LowerBound"][order], s[!, "UpperBound"][order]),
+    label="$(o) cf", color=:green)
 
 xlabel!("Temperature °F")
 ylabel!("Energy Consumption")
